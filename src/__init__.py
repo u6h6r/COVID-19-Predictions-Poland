6c0fd06b76a0cfc0723ff2bab07c774799807294
u6h6r <filip.uherek4@gmail.com>
2020-12-21 17:49:23 +0100

    Updates init file

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
-from .data_processing import prepare_europe_data, prepare_empty_frame, plot_covid_data
+from .data_processing import prepare_europe_data, prepare_empty_frame, plot_covid_data, get_daily_cases
+from .statistical_tools import tukey_outliers, crosscorr
